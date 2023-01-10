b421ba2933e4543e8c0625fcc71e8bed6ec191d5
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-01-10 17:47:09 +0500

    Add mapper func to Postgres Entity

--- infrastructure/db/postgres/entities/legoseries.go
+++ infrastructure/db/postgres/entities/legoseries.go
@@ -1,4 +1,16 @@
 package postgres
 
+import (
+	"legocy-go/pkg/lego/models"
+)
+
 type LegoSeriesPostgres struct {
+	Name string
+}
+
+func (s *LegoSeriesPostgres) ToLegoSeries() *models.LegoSeries {
+	return &models.LegoSeries{
+		ID:   -1,
+		Name: s.Name,
+	}
 }
