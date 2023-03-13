0e4e6413e029b6a1dd0d817f766182b3f1b8cc7c
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-03-14 01:10:11 +0500

    update config filepath

--- cmd/legocy-go/main.go
+++ cmd/legocy-go/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	r "legocy-go/delievery/http/router"
 	"legocy-go/internal/app"
+	"os"
 )
 
-const configFilepath = "/Users/wjojf/GolandProjects/legocy-go-clean/internal/config/json/config.json"
+const configFilepath = "/internal/config/json/config.json"
 
 func main() {
-	_app := app.New(configFilepath)
+	cwd, _ := os.Getwd()
+	_app := app.New(cwd + configFilepath)
 
 	v1 := r.InitRouter(_app)
 	v1.Run("8080")
