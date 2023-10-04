4d48f6581a1897664cdb1e219c2bc4d5b3e90296
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-10-04 17:38:49 +0200

    collection valuation service method done

--- internal/domain/collections/repository/legoSetValuationRepository.go
+++ internal/domain/collections/repository/legoSetValuationRepository.go
@@ -8,6 +8,7 @@ import (
 type LegoSetValuationRepository interface {
 	GetLegoSetValuationsList(c context.Context, legoSetID int) ([]models.LegoSetValuation, error)
 	GetLegoSetValuationByID(c context.Context, id int) (*models.LegoSetValuation, error)
+	GetLegoSetValuationBySetStateCurrency(c context.Context, setID int, setState string, currencyID int) (*models.LegoSetValuation, error)
 	AddLegoSetValuation(c context.Context, vo models.LegoSetValuationValueObject) error
 	DeleteLegoSetValuationByID(c context.Context, id int) error
 	UpdateLegoSetValuationByID(c context.Context, id int, vo models.LegoSetValuationValueObject) error
--- internal/domain/collections/service/collection/service.go
+++ internal/domain/collections/service/collection/service.go
@@ -7,25 +7,49 @@ import (
 )
 
 type UserCollectionService struct {
-	r repository.UserCollectionRepository
+	collectionRepository repository.UserCollectionRepository
+	valuationRepository  repository.LegoSetValuationRepository
 }
 
 func NewUserCollectionService(r repository.UserCollectionRepository) UserCollectionService {
-	return UserCollectionService{r: r}
+	return UserCollectionService{collectionRepository: r}
 }
 
 func (s UserCollectionService) GetUserCollection(c context.Context, userID int) (*models.LegoCollection, error) {
-	return s.r.GetUserCollection(c, userID)
+	return s.collectionRepository.GetUserCollection(c, userID)
 }
 
 func (s UserCollectionService) AddSetToUserCollection(c context.Context, userID int, vo models.CollectionLegoSetValueObject) error {
-	return s.r.AddSetToUserCollection(c, userID, vo)
+	return s.collectionRepository.AddSetToUserCollection(c, userID, vo)
 }
 
 func (s UserCollectionService) RemoveSetFromUserCollection(c context.Context, userID int, collectionSetID int) error {
-	return s.r.RemoveSetFromUserCollection(c, userID, collectionSetID)
+	return s.collectionRepository.RemoveSetFromUserCollection(c, userID, collectionSetID)
 }
 
 func (s UserCollectionService) UpdateUserCollectionSet(c context.Context, userID int, collectionSetID int, vo models.CollectionLegoSetValueObject) error {
-	return s.r.UpdateUserCollectionSetByID(c, userID, collectionSetID, vo)
+	return s.collectionRepository.UpdateUserCollectionSetByID(c, userID, collectionSetID, vo)
+}
+
+func (s UserCollectionService) GetUserCollectionValuation(c context.Context, userID int, currencyID int) ([]models.LegoSetValuation, error) {
+	userCollection, err := s.GetUserCollection(c, userID)
+	if err != nil {
+		return []models.LegoSetValuation{}, err
+	}
+
+	var setValuations []models.LegoSetValuation
+
+	for _, userLegoSet := range userCollection.Sets {
+		setValuation, err := s.valuationRepository.GetLegoSetValuationBySetStateCurrency(
+			c, userLegoSet.LegoSet.ID, userLegoSet.CurrentState, currencyID,
+		)
+
+		if err != nil {
+			return []models.LegoSetValuation{}, err
+		}
+
+		setValuations = append(setValuations, *setValuation)
+	}
+
+	return setValuations, nil
 }
