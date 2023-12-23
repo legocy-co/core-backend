74b6dd85e03320bf64ed3f1e48824ddd134132b0
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-12-23 22:06:35 +0100

    wip marketItemImage

--- internal/domain/marketplace/repository/market_item_image.go
+++ internal/domain/marketplace/repository/market_item_image.go
@@ -1,8 +1,12 @@
 package marketplace
 
-import models "github.com/legocy-co/legocy/internal/domain/marketplace/models"
+import (
+	"github.com/legocy-co/legocy/internal/app/errors"
+	models "github.com/legocy-co/legocy/internal/domain/marketplace/models"
+)
 
 type MarketItemImageRepository interface {
-	Store(vo models.MarketItemImageValueObject) (*models.MarketItemImage, error)
+	Get(marketItemID int) ([]*models.MarketItemImage, *errors.AppError)
+	Store(vo models.MarketItemImageValueObject) (*models.MarketItemImage, *errors.AppError)
 	Delete(id int) error
 }
