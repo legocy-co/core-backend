089f385286b5e1c58a574b8933819b01741eccf4
Tikhon Kozlov <STPKBFF@yandex.ru>
2024-01-23 18:49:58 +0100

    debug

--- internal/delivery/http/handlers/users/userImage/user_image_list.go
+++ internal/delivery/http/handlers/users/userImage/user_image_list.go
@@ -20,13 +20,13 @@ import (
 func (h UserImageHandler) ListImages(c *gin.Context) {
 	userID, err := strconv.Atoi(c.Param("userID"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error1": err.Error()})
 		return
 	}
 
 	imagesList, err := h.service.GetUserImages(c, userID)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error2": err.Error()})
 	}
 
 	imageResponseList := make([]schemas.UserImageInfoResponse, len(imagesList))
