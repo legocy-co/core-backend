902bb5d3797ac1d9fff52cac8baf7e46c905d11b
ereng <erengkorkmaz@gmail.com>
2024-03-07 12:59:53 +0100

    fiz logrus?

--- internal/delivery/http/router/v1/admin/admin_market_item.go
+++ internal/delivery/http/router/v1/admin/admin_market_item.go
@@ -2,20 +2,32 @@ package admin
 
 import (
 	"github.com/gin-gonic/gin"
+	a "github.com/legocy-co/legocy/internal/app"
 	h "github.com/legocy-co/legocy/internal/delivery/http/handlers/marketplace/admin/marketItem"
-	s "github.com/legocy-co/legocy/internal/domain/marketplace/service/admin"
+	"github.com/legocy-co/legocy/internal/delivery/http/handlers/marketplace/image"
 	"github.com/legocy-co/legocy/pkg/auth/jwt/middleware"
 )
 
-func AddAdminMarketItems(rg *gin.RouterGroup, service s.MarketItemAdminService) {
-	handler := h.NewMarketItemAdminHandler(service)
+func AddAdminMarketItems(rg *gin.RouterGroup, app *a.App) {
 
 	marketItemsAdmin := rg.Group("/market-items").Use(middleware.IsAdmin())
 	{
-		marketItemsAdmin.GET("/", handler.GetMarketItemsAdmin)
-		marketItemsAdmin.GET("/:itemId", handler.GetMarketItemByID)
-		marketItemsAdmin.POST("/", handler.CreateMarketItem)
-		marketItemsAdmin.PUT("/:itemId", handler.UpdateMarketItemByID)
-		marketItemsAdmin.DELETE("/:itemId", handler.DeleteMarketItemById)
+		handler := h.NewMarketItemAdminHandler(app.GetMarketItemAdminService())
+		{
+			marketItemsAdmin.GET("/", handler.GetMarketItemsAdmin)
+			marketItemsAdmin.GET("/:itemId", handler.GetMarketItemByID)
+			marketItemsAdmin.POST("/", handler.CreateMarketItem)
+			marketItemsAdmin.PUT("/:itemId", handler.UpdateMarketItemByID)
+			marketItemsAdmin.DELETE("/:itemId", handler.DeleteMarketItemById)
+		}
+	}
+
+	marketItemImagesAdmin := rg.Group("/market-items/images").Use(middleware.IsAdmin())
+	{
+		handler := image.NewHandler(app.GetMarketItemImageService(), app.GetImageStorageClient())
+		{
+			marketItemImagesAdmin.POST("/:marketItemID", handler.UploadImage)
+			marketItemImagesAdmin.DELETE("/:imageId", handler.Delete)
+		}
 	}
 }
