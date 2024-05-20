e627b5442aad510718b0031fe8993eb5530cec16
Tikhon Kozlov <STPKBFF@yandex.ru>
2024-05-20 18:22:04 +0200

    hotfix google test

--- internal/delivery/http/handlers/users/auth/google/handler_test.go
+++ internal/delivery/http/handlers/users/auth/google/handler_test.go
@@ -13,6 +13,11 @@ func TestTokenValidation(t *testing.T) {
 	appID := os.Getenv("GOOGLE_CLIENT_ID")
 	token := os.Getenv("GOOGLE_TEST_TOKEN")
 
+	if token == "" {
+		log.Println("empty token. Skipping test...")
+		return
+	}
+
 	payload, err := idtoken.Validate(context.Background(), token, appID)
 	if err != nil {
 		t.Errorf(err.Error())
