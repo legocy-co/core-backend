742be91c9fa1c95346d821b79738d4007cef8269
Tikhon Kozlov <STPKBFF@yandex.ru>
2024-06-24 23:33:54 +0200

    fill in random password helper

--- lib/helpers/password.go
+++ lib/helpers/password.go
@@ -1,5 +1,21 @@
 package helpers
 
+import (
+	"math/rand"
+)
+
+const (
+	letterBytes    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numberBytes    = "0123456789"
+	specialBytes   = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
+	allBytes       = letterBytes + numberBytes + specialBytes
+	passwordLength = 12 // Change this to your desired password length
+)
+
 func GetRandomPassword() string {
-	return ""
+	b := make([]byte, passwordLength)
+	for i := range b {
+		b[i] = allBytes[rand.Intn(len(allBytes))]
+	}
+	return string(b)
 }
